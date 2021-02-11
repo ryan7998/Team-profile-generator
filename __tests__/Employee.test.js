e24864c43df802e4f718d71fb0154bf25471fafc
fazle <ryan7998@gmail.com>
2021-02-11 18:17:53 -0500

    parent class created and tested

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee.js');
+
+test('creates a Employee Object', ()=>{
+    const employee = new Employee('Fazle', '101', 'mail@mail.com');
+
+    expect(employee.name).toBe('Fazle');
+    expect(employee.id).toBe('101');
+    expect(employee.email).toBe('mail@mail.com');
+});
+
+test('get name from function getName()', ()=>{
+    const employee = new Employee('Fazle', '101', 'mail@mail.com');
+
+    expect(employee.getName()).toBe('Fazle');
+});
+
+test('get id from function getId()', ()=>{
+    const employee = new Employee('Fazle', '101', 'mail@mail.com');
+
+    expect(employee.getId()).toBe('101');
+});
+
+test('get email from function getEmail()', ()=>{
+    const employee = new Employee('Fazle', '101', 'mail@mail.com');
+
+    expect(employee.getEmail()).toBe('mail@mail.com');
+});
+
+test('get class name from function getRole()', ()=>{
+    const employee = new Employee('Fazle', '101', 'mail@mail.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
